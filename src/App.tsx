5af6fe07b8b0d288efe471a73825f46458697ccb
John Souza <johnpeterisreal716@gmail.com>
2025-07-21 21:21:21 -0400

    The first step is making it exist

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">It needs to exist</h1>
+
+        <p>It can be made good later</p>
+       </div>
+    </>
+  )
+}
+
+export default App
